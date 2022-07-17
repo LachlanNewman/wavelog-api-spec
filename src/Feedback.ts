7df981ec4aaf0b9ff7e0ecfe8bae54f9cb165e4f
LachlanNewman <lachnewman007@gmail.com>
2022-07-17 11:25:29 +0800

    fix action

---
+++ src/Feedback.ts
@@ -0,0 +1,32 @@
+import Joi from "joi";
+import { genAlphaNumeric, genNumber, genSample } from "./Gen";
+import { getEncodeDecodePair } from "./Joi";
+
+export interface FeedbackRecord extends Feedback{
+    id: string
+}
+
+export interface Feedback{
+    rating: number;
+    difficulty: number;
+    fun: number;
+    scary: number;
+}
+
+export const genFeedBack = ():Feedback => ({
+    rating: genNumber(),
+    difficulty: genNumber(),
+    fun: genNumber(),
+    scary: genNumber(),
+})
+
+
+export const FeedbackSchema = Joi.object({
+    rating: Joi.number().required(),
+    difficulty: Joi.number().required(),
+    fun: Joi.number().required(),
+    scary: Joi.number().required()
+})
+
+export const { decoder: decodeFeedback, encoder: encodeFeedback } =
+  getEncodeDecodePair<Feedback>(FeedbackSchema);
